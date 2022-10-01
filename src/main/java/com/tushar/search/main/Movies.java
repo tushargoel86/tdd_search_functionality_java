bd38e63c44f846998585fcc60a2084c1e5b09317
Tushar Goel <tushargoel86@gmail.com>
2022-10-01 16:18:54 +0530

    Test case fixes for movie starting with some characters in same case.

--- src/main/java/com/tushar/search/main/Movies.java
+++ src/main/java/com/tushar/search/main/Movies.java
@@ -1,9 +1,24 @@
 package com.tushar.search.main;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Movies {
-    public List<String> search(String word) {
-        return List.of();
+    private Set<String> movieList;
+
+    public Movies() {
+        this.movieList = new HashSet<>();
     }
+
+    public List<String> search(String word) {
+        return movieList.stream().filter(name -> name.startsWith(word)).collect(Collectors.toList());
+    }
+
+    public void addMovie(String movie) {
+        this.movieList.add(movie);
+    }
+
 }
--- src/test/java/com/tushar/search/main/MovieTest.java
+++ src/test/java/com/tushar/search/main/MovieTest.java
@@ -1,5 +1,6 @@
 package com.tushar.search.main;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,21 +10,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MovieTest {
 
+    private Movies movies;
+
+    @BeforeEach
+    void setup() {
+        this.movies = new Movies();
+        addData();
+    }
+
     @Test
     @DisplayName("Should not return any movie when movie name length less than 2")
     void testForMovieNameLengthLessThanTwo() {
-        Movies movie = new Movies();
-        List<String> movies = movie.search("a");
+        List<String> movieList = movies.search("a");
 
-        assertEquals(0, movies.size());
+        assertEquals(0, movieList.size());
     }
 
     @Test
     @DisplayName("Should return 2 movies when movie name starts with Va")
     void testForMovieNameLengthEqualsToTwo() {
-        Movies movie = new Movies();
-        List<String> movies = movie.search("Va");
+        List<String> movieList = movies.search("Va");
 
-        assertEquals(2, movies.size());
+        assertEquals(2, movieList.size());
+    }
+
+    private void addData() {
+      List<String> movieList = List.of("Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver",
+                "Amsterdam", "Vienna", "Sydney", "New York City", "London",
+          "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul");
+      movieList.stream().forEach(movies::addMovie);
     }
 }
